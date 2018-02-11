6a01f2acfb6655f9e94d8ed94cfd0c3ac6daef9c
Logan Corley <capt.smitty77@gmail.com>
2018-02-10 22:49:30 -0500

    Added end-game conditions

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const rowCount = 20;
 const columnCount = 20;
 const tileSize = 20;
 const tileSpacing = 2;
-const mineCount = 50;
+const mineCount = 40;
 
 var tiles = [];
 for (var i = 0; i < columnCount; i++) {
     tiles.push([]);
 }
+
+var markerCount = 0;
 
 class Tile {
     constructor(x, y, hasMine) {
@@ -15,6 +17,7 @@ class Tile {
         this.x = x;
         this.y = y;
         this.exposed = false;
+        this.marked = false;
         this.text = hasMine ? 'X' : ' ';
 
         tiles[x][y] = this;
@@ -33,12 +36,17 @@ class Tile {
         this.tileDiv.style.top = ((tileSize + tileSpacing) * y)+"px";
         this.tileDiv.style.border = tileSpacing+"px solid #fff";
         
-        this.tileDiv.onmousedown = tileClicked;
+        this.tileDiv.addEventListener("mousedown", tileClicked);
+        this.tileDiv.oncontextmenu = () => { return false; }; // Prevent context menu from making an appearance
     }
 
     expose() {
         this.exposed = true;
         this.tileDiv.setAttribute("class", "tile exposed-tile");
+
+        if (this.marked) {
+            this.tileDiv.innerHTML = this.hasMine ? '@' : this.adjacentCount;
+        }
 
         switch (this.tileDiv.innerHTML) {
             case '0': this.tileDiv.style.color = "transparent"; break;
@@ -51,6 +59,23 @@ class Tile {
             case '7': this.tileDiv.style.color = "black"; break;
             case '8': this.tileDiv.style.color = "grey"; break;
             default: break;
+        }
+    }
+
+    mark() {
+        if (this.exposed) return;
+
+        if (!this.marked) {
+            this.marked = true;
+            this.tileDiv.style.color = "red";
+            this.tileDiv.innerHTML = '#';
+            markerCount++;
+        }
+        else {
+            this.marked = false;
+            this.tileDiv.style.color = "transparent";
+            this.tileDiv.innerHTML = this.hasMine ? '@' : this.adjacentCount;
+            markerCount--;
         }
     }
 
@@ -76,8 +101,6 @@ class Tile {
     }
 }
 
-populateBoard();
-
 function populateBoard() {
     for (var y = 0; y < rowCount; y++) {
         for (var x = 0; x < columnCount; x++) {
@@ -101,26 +124,80 @@ function populateBoard() {
     // Count mines
     for (var y = 0; y < rowCount; y++) {
         for (var x = 0; x < columnCount; x++) {
-            let currentTile = tiles[y][x];
+            let currentTile = tiles[x][y];
 
             if (!currentTile.hasMine) {
                 currentTile.divText = currentTile.adjacentCount;
             }
             else {
-                currentTile.divText = 'X';
+                currentTile.divText = '@';
             }
         }
     }
     
 }
 
+function showGameOver() {
+    disableBoard();
+
+    for (var y = 0; y < rowCount; y++) {
+        for (var x = 0; x < columnCount; x++) {
+            let tile = tiles[x][y];
+
+            if (tile.hasMine) {
+                if (tile.marked) tile.divText = '@'; // Reset mark to mine
+
+                tile.tileDiv.style.color = "black";
+            }
+        }
+    }
+}
+
+function checkForWin() {
+    let win = true;
+
+    for (var y = 0; y < rowCount; y++) {
+        for (var x = 0; x < columnCount; x++) {
+            let tile = tiles[x][y];
+
+            if (!tile.hasMine && tile.marked) win = false;
+        }
+    }
+
+    if (win) {
+        disableBoard();
+        alert ("You Win!");
+    }
+}
+
+function disableBoard() {
+    for (var y = 0; y < rowCount; y++) {
+        for (var x = 0; x < columnCount; x++) {
+            let tile = tiles[x][y];
+            tile.tileDiv.removeEventListener("mousedown", tileClicked);
+        }
+    }
+}
+
 function tileClicked(e) {
-    let tile = e.target;
-    exposeTile(tiles[tile.getAttribute("data-x")][tile.getAttribute("data-y")]);
+    e.preventDefault();
+    
+    let tile = tiles[e.target.getAttribute("data-x")][e.target.getAttribute("data-y")];
+
+    if (e.button == 0) {
+        exposeTile(tile);
+    }
+    else if (e.button == 2) {
+        tile.mark();
+
+        if (markerCount == mineCount) checkForWin();
+    }
 }
 
 function exposeTile(tile) {
     tile.expose();
+
+    if (tile.hasMine) showGameOver();
     
     if (tile.adjacentCount == 0) {
         for (var i = -1; i <= 1; i++) {
@@ -137,3 +214,5 @@ function exposeTile(tile) {
         }
     }
 }
+
+populateBoard();
